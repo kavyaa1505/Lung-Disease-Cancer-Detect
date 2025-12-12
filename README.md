5187384d7c2deceaccb60faf3aa4197a5dca2229
ashu-nair <ashithoshnair669@gmail.com>
2025-12-12 16:56:01 +0530

    Trigger deployment

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # Deployment Branch for ML API
 # Trigger
 deploy fix
+update
--- scripts/copy_models.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir -p ml_models/models
-cp -r ml_branch/models/* ml_models/models/
-echo "Model files copied."
-
